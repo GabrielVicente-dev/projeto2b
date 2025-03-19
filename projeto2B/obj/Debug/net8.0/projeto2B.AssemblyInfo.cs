3073ece8ff65463341d32fa93c77251df80cedb9
gabriel <gabz07santos@gmail.com>
2025-03-19 20:44:54 -0300

    criando a estruturas de pasta do projeto

--- projeto2B/obj/Debug/net8.0/projeto2B.AssemblyInfo.cs
+++ projeto2B/obj/Debug/net8.0/projeto2B.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("projeto2B")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd14c32881735993dd3f9c827ce753f29d2b187c")]
 [assembly: System.Reflection.AssemblyProductAttribute("projeto2B")]
 [assembly: System.Reflection.AssemblyTitleAttribute("projeto2B")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
