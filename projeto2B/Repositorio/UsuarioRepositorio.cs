fd8560ff7f34d4a6457d41ebfcefdbc2be21a9d6
Gabriel <gabz07santos@gmail.com>
2025-03-21 22:13:25 -0300

    Criando o método Obter

--- projeto2B/Repositorio/UsuarioRepositorio.cs
+++ projeto2B/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,34 @@
-﻿namespace projeto2B.Repositorio
+﻿using projeto2B.Models;
+
+
+namespace projeto2B.Repositorio
 {
     public class UsuarioRepositorio
     {
+        //preparando a estrutura da conexão com o Banco de Dados 
+
+        private readonly string _connectionString;
+
+        //Construtor
+        public UsuarioRepositorio(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+        // Criando o método adicionar usuario
+        public void AdicionarUsuario(Usuario usuario)
+        {
+            using (var db = new Conexao(_connectionString))
+            {
+                var cmd = db.MySqlCommand();
+                cmd.CommandText = "INSERT INTO Usuario (Nome, Email, Senha) VALUES (@Nome,@Email,@Senha)";
+                cmd.Parameters.AddWithValue("@Nome", usuario.Nome);
+                cmd.Parameters.AddWithValue("@Email", usuario.Email);
+                cmd.Parameters.AddWithValue("@Senha", usuario.Senha);
+                cmd.ExecuteNonQuery();
+
+            }
+        }
+
+
     }
 }
